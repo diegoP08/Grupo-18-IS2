c7c223d5d7de0d4c674740093b7a673e0e8d96b8
Diego Prince <diegoprince08@hotmail.com>
2018-05-08 18:14:43 -0300

    version inicial

---
+++ application/views/barraSuperior.php
@@ -0,0 +1,64 @@
+<?php
+  session_start();
+  if (isset($_SESSION['email'])){
+?>
+<!-- Barra de que se muestra si esta logueado -->
+<div class="container-fluid" style="background-color: black">
+  <div class="container">
+    <div class="row">
+      <div class="col-3">
+        <a href="<?php echo base_url() ?>inicio" class="navbar-brand">
+          <img src="<?php echo base_url() ?>assets/img/LogoA.jpg" style="width: 100px">
+        </a>
+      </div>
+      <div class="col-9">
+        <div class="row justify-content-end" style="padding-right:15px;padding-top: 12px">
+          <a href="#"><img src="<?php echo base_url() ?>assets/img/usuario.jpg" style="height: 40px; width: 40px"></a>
+          <div class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <?php echo($_SESSION['nombre'] . " " . $_SESSION['apellido']) ?>
+            </a>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="#">Perfil</a>
+              <a class="dropdown-item" href="#">Viajes</a>
+              <a class="dropdown-item" href="#">Pagos</a>
+            </div>
+          </div>
+        </div>
+        <ul class="nav">
+          <li class="nav-item"><a href="<?php echo base_url() ?>inicio" class="nav-link"> <h5 >INICIO</h5></a> </li>
+          <li class="nav-item"><a href="#" class="nav-link"> <h5>SOBRE NOSOTROS</h5> </a> </li>
+          <li class="nav-item"><a href="#" class="nav-link"> <h5>CONTACTO</h5> </a> </li>
+          <li class="nav-item"><a href="#" class="nav-link"> <h5>BUSCAR VIAJE</h5> </a> </li>
+          <li class="nav-item"></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Barra que se muestra si no esta logeado -->
+<?php }else{ ?>
+<div class="container" style="background-image:url(<?php echo base_url() ?>assets/img/fondo-translucido.png);">
+  <div class="row">
+    <div class="col-2">
+      <a href="<?php echo base_url() ?>" class="navbar-brand">
+        <img src="<?php echo base_url() ?>assets/img/LogoA.jpg" style="width: 100px">
+      </a>
+    </div>
+    <div class="col-10">
+      <div class="row justify-content-end">
+        <div style="margin-top: 10px; margin-right: 10px">
+          <a class="btn btn-primary" style="background-color: #f37277; border-color:#f37277" href="<?php echo base_url() ?>registro">REGISTRARSE</a>
+          <a class="btn btn-primary" style="background-color: #f37277; border-color:#f37277" href="<?php echo base_url() ?>ingreso">INICIAR SESIÓN</a>
+        </div>
+      </div>
+      <ul class="nav">
+        <li class="nav-item"> <a href="<?php echo base_url() ?>" class="nav-link" style="color: #f37277"> <h5>INICIO</h5></a> </li>
+        <li class="nav-item"> <a href="#" class="nav-link" style="color: #f37277"> <h5>SOBRE NOSOTROS</h5> </a> </li>
+        <li class="nav-item"> <a href="#" class="nav-link" style="color: #f37277"> <h5>CONTACTO</h5> </a> </li>
+        <li class="nav-item"> <a href="#" class="nav-link" style="color: #f37277"> <h5>BUSCAR VIAJE</h5> </a> </li>
+      </ul>
+    </div>
+  </div>
+</div>
+<?php } ?>
---
+++ application/views/start.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html style="height: 100%;">
+	<head>
+		<title> Un Aventón </title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="with=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta http-equiv="x-ua-compatible" content="ie-edge">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap.css">
+		<link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url() ?>assets/img/icono.ico">
+	</head>
+
+	<body style="background-image: url(<?php echo base_url() ?>assets/img/fondo2.jpg); background-repeat:repeat;height: 100%;">
+		<?php require "barraSuperior.php" ?>
+
+		<div class="container" style="background-image:url(<?php echo base_url() ?>assets/img/fondo-translucido.png);color: white;height: 100%;">
+			<br>
+			<div class="row">
+				<!-- Listado de objetivos -->
+				<div class="col">
+					<h3 align="center">¿Estas buscando compartir un viaje?</h3>
+					<h4>En esta pagina te ofrecemos:</h4>
+					<div>
+						<ul>
+							<li><h5>Acompañar a un conductor</h5></li>
+							<li><h5>Publicar un viaje</h5></li>
+							<li><h5>Buscar viajes para ofrecerte como acompañante</h5></li>
+							<li><h5>Pagar los viajes en los que vas a participar</h5></li>
+							<li><h5>Recibir los pagos de tus acompañantes</h5></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<script src="<?php echo base_url() ?>assets/js/jquery-3.2.1.slim.min.js"></script>
+		<script src="<?php echo base_url() ?>assets/js/popper.min.js"></script>
+		<script src="<?php echo base_url() ?>assets/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url() ?>assets/js/jquery-3.3.1.min.js"></script>
+	</body>
+</html>
