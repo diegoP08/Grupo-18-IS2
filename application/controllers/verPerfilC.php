1d554b339611d91767f3886d7aecd3a344380d65
Matias Caballero <matiitas.caballero99@gmail.com>
2018-05-15 17:40:00 -0300

    Minimo arreglo en vehiculos e interface de ver perfil agregado

---
+++ application/controllers/verPerfilC.php
@@ -0,0 +1,7 @@
+<?php
+class VerPerfilC extends CI_Controller {
+
+	public function index(){
+		$this->load->view('verPerfilV');
+	}
+}
---
+++ application/views/verPerfilV.php
@@ -0,0 +1,10 @@
+<?php if (! isset($_SESSION['email'])){redirect("start");} ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr" style="height:100%">
+  <?php require "head.php" ?>
+  <body style="background-image: url(<?php echo base_url() ?>assets/img/fondo1.png); background-repeat:repeat;height: 100%;">
+      <div class="container" style="background: rgba(0,0,0,0.5); box-shadow: 0 0 10px 3px black; min-height: 100%;">
+        <?php require "barraSuperior.php" ?>
+      </div>
+  </body>
+</html>
