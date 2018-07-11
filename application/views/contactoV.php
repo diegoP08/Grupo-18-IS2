01af5be406ada5588f6d2a937a233409022aadb5
Matias Caballero <matiitas.caballero99@gmail.com>
2018-07-11 19:43:49 -0300

    Agregado lo de contacto

--- application/views/contactoV.php
+++ application/views/contactoV.php
@@ -1,9 +1,51 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html lang="en" dir="ltr" style="height: 100%">
   <?php require 'head.php' ?>
   <body style="background-image: url(<?php echo base_url() ?>assets/img/fondo1.png); background-repeat:repeat;height: 100%;">
-    <div class="container" style="background: rgba(0,0,0,0.5); box-shadow: 0 0 10px 3px black; min-height: 100%;">
+    <div class="container" style="background: rgba(0,0,0,0.5); box-shadow: 0 0 10px 3px black; min-height: 100%">
       <?php require 'barraSuperior.php' ?>
+      <br>
+      <div class="container" style="background: rgba(0,26,26,0.5);width: 90%;box-shadow: 0px 0px 10px 4px black;">
+        <h1 align='center' style="color:white; padding: 10px;">Contacto</h1>
+        <br>
+        <div class="container">
+          <div class="row" style="color: white">
+            <div class="col-sm" style="">
+              <h3 align="center" style="color:#f37277">Nuestra oficina</h3>
+              <br>
+                <ul>
+                  <p>SyNTar S.R.L</p>
+                  <p>Sistemas y Nuevas Tecnologias Argentina</p>
+                  <p>Calle 50 y 120, La Plata, Buenos Aires</p>
+                  <p>Telefono: (0223) 473-9447</p>
+                  <p><a itemprop="url" href="http://localhost/unaventon/index.php/start/">www.unaventon.com.ar</a></p>
+                </ul>
+            </div>
+            <div class="col-sm">
+              <h3 align="center" style="color:#f37277">Servicio al cliente</h3>
+              <br>
+                <ul>
+                  <p>Horarios de atención:</p>
+                    <p>En oficina:</p>
+                    <p><li style="margin-left: 25px">9:00am - 17:00pm</li></p>
+                    <p>Atención online:</p>
+                    <p><li style="margin-left: 25px">24/7</li></p>
+                    <p>Email: <li style="margin-left: 25px"><a href="mailto:atencion_cliente@unaventon.com" target="_blank">atencion_cliente@unaventon.com</a></li></p>
+                </ul>
+            </div>
+            <div class="col-sm">
+              <h3 align="center" style="color:#f37277">Otros contactos</h3>
+              <br>
+                <ul>
+                  <p>Desarrolladores:</p>
+                    <p style="margin-left: 10px">Emails: <li style="margin-left: 25px"><a href="mailto:diego.di.giorgio@unaventon.com" target="_blank">diego.di.giorgio@unaventon.com</a></li></p>
+                    <p> <li style="margin-left: 25px"><a href="mailto:diego.prince@unaventon.com" target="_blank">diego.prince@unaventon.com</a></li></p>
+                    <p> <li style="margin-left: 25px"><a href="mailto:matias.caballero@unaventon.com" target="_blank">matias.caballero@unaventon.com</a></li></p>
+                </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </body>
   <?php require 'scripts.php' ?>
--- application/views/sobreNosotrosV.php
+++ application/views/sobreNosotrosV.php
@@ -1,9 +1,26 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html lang="en" dir="ltr" style="height: 100%">
   <?php require 'head.php' ?>
   <body style="background-image: url(<?php echo base_url() ?>assets/img/fondo1.png); background-repeat:repeat;height: 100%;">
-    <div class="container" style="background: rgba(0,0,0,0.5); box-shadow: 0 0 10px 3px black; min-height: 100%;">
+    <div class="container" style="background: rgba(0,0,0,0.5); box-shadow: 0 0 10px 3px black; min-height: 100%">
       <?php require 'barraSuperior.php' ?>
+      <br>
+      <div class="container" style="background: rgba(0,26,26,0.5);width: 90%;box-shadow: 0px 0px 10px 4px black;">
+        <h1 align='center' style="color:white; padding: 10px;">Ayuda</h1>
+        <p><h5 style="color:white">En esta sección tendrás las respuestas a las preguntas más realizadas por los usuarios, que servirán para
+            tener una idea más clara de como funciona el sitio y las acciones que puedas realizar en el.</h5></p>
+            <div class="container">
+              <div class="row">
+                <div class="col-sm">
+                  <h6></h6>
+                </div>
+                <div class="col-sm">
+
+                </div>
+              </div>
+            </div>
+        <br>
+      </div>
     </div>
   </body>
   <?php require 'scripts.php' ?>
